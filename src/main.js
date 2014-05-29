ffb2bc0ef0a9ccbb698b81ad87288fa2269f6d9c
unknown <towenmilner@gmail.com>
2014-05-29 11:48:27 -0700

    Started on the project

---
+++ src/main.js
@@ -0,0 +1,17 @@
+function Main(){
+	
+	//place initialization code here
+	//and here
+	
+	this.x = 3;
+
+}
+
+Main.prototype.update = function(){
+	
+	//console.log(this.x);
+}
+
+Main.prototype.getX = function(){
+	return this.x;
+};
