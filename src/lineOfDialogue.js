bc1baadd3f6934723e1487134e7f87e6649c2f16
unknown <towenmilner@gmail.com>
2014-05-30 14:48:08 -0700

    Finished the baseline for what it should look like, as well as some minimum interactiveness

---
+++ src/lineOfDialogue.js
@@ -0,0 +1,17 @@
+function LineOfDialogue(lineNum){
+    this.lineNumber = lineNum;
+
+    this.speaker = "";
+
+    this.text = "";
+
+
+    //Put Annotation properties here
+
+
+}
+
+LineOfDialogue.prototype.update = function(){
+
+
+};
--- src/main.js
+++ src/main.js
@@ -1,17 +1,53 @@
 function Main(){
-	
+
 	//place initialization code here
 	//and here
-	
-	this.x = 3;
+
+	this.currentLineNumber = 0;
+
+	this.linesOfDialogue = [];
+	this.linesOfDialogue.push(new LineOfDialogue(1));
 
 }
 
 Main.prototype.update = function(){
-	
-	//console.log(this.x);
-}
-
-Main.prototype.getX = function(){
-	return this.x;
+	for(var lineNum = 0; lineNum < this.linesOfDialogue.length; lineNum++){
+        var line = this.linesOfDialogue[lineNum];
+        line.update();
+	}
 };
+
+Main.prototype.addLine = function(){
+    this.currentLineNumber++;
+    this.linesOfDialogue.push(new LineOfDialogue(this.currentLineNumber));
+};
+
+
+
+
+/*
+Event listeners and html/jquery code goes here
+*/
+$(function(){
+
+    $(".dropdown-menu").on('click', 'li a', function(){
+      $(".btn:first-child").text($(this).text());
+      $(".btn:first-child").val($(this).text());
+   });
+
+});
+
+$(function(){
+    $(".list-group").on('click', 'a', function(){
+        var div = $(".list-group:parent");
+        var div2 =  $("a");
+        var partOne = "<li class='list-group-item'> <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button> ";
+        var insides = "Blue blue blue";
+        var partTwo = " </li> <a href='#' class='list-group-item'>+</a>";
+        div2.remove();
+        div.append(partOne + insides + partTwo);
+    });
+
+
+
+});
