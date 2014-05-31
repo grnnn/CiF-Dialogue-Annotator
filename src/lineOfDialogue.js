13c9d8d9aa0ec5994c07b7c350ccacf4dc00fb76
unknown <towenmilner@gmail.com>
2014-05-30 23:28:11 -0700

    Finished the logic for adding new lines/properties dynamically

--- src/lineOfDialogue.js
+++ src/lineOfDialogue.js
@@ -1,16 +1,107 @@
 function LineOfDialogue(lineNum){
-    this.lineNumber = lineNum;
+
+    //Class to represent each line of dialogue
+
+    this.lineNumber = lineNum.toString();
 
     this.speaker = "";
 
     this.text = "";
 
 
-    //Put Annotation properties here
+    //Initialize Base HTML
+    this.baseStructureConfigure();
+
+    //Initialize Annotation Properties
+    this.propertyDropdownConfigure();
 
 
 }
 
+//Append base structure
+LineOfDialogue.prototype.baseStructureConfigure = function(){
+    //Superlong append that I wish I could make tidier
+    $('#LinesContainer').append("<div class='row' ><div class='col-md-2' ><div class='dropdown'><button type='button' class='btn btn-default dropdown-toggle'  id='SpeakerDropDownButton"+this.lineNumber+"' data-toggle='dropdown'>Select Speaker <span class='caret'></span></button><ul class='dropdown-menu' role='menu' aria-labelledby='dropdownMenu1' id='SpeakerDropDown"+this.lineNumber+"'><li role='presentation'><a role='menuitem' tabindex='-1' >Initiator</a></li><li role='presentation'><a role='menuitem' tabindex='-1' >Responder</a></li><li role='presentation'><a role='menuitem' tabindex='-1' >Outsider</a></li></ul></div></div><div class='col-md-5' ><textarea rows='5' cols='72' style='resize:vertical' id='TextArea"+this.lineNumber+"'>Enter Dialogue here</textarea></div><div class='col-md-5' id='Properties"+this.lineNumber+"' ></div></div><div class='page-header'></div>");
+
+    //'click' listener for the Speaker dropdowns
+    $("#SpeakerDropDown"+this.lineNumber).on('click', 'li a', function(){
+        //get the line number
+        var lineNum =  $(this).parent().parent().attr("id").replace("SpeakerDropDown", "");
+
+
+        //Set the text in the Dropdown button to the selected text
+        $("#SpeakerDropDownButton"+lineNum).text($(this).text());
+        $("#SpeakerDropDownButton"+lineNum).val($(this).text());
+    });
+
+}
+
+//Append the properties, and the listeners for those properties
+LineOfDialogue.prototype.propertyDropdownConfigure = function(){
+    //Set all of the text for the property dropdown menu, loop through each type of property to get it
+    var textForDropdown = "<div class='dropdown' id='PropertiesDropDown"+this.lineNumber+"' style='padding:15px'><button type='button' class='btn btn-default dropdown-toggle'   data-toggle='dropdown'>+ Add Type of Annotation <span class='caret'></span></button> <ul class='dropdown-menu' role='menu' aria-labelledby='dropdownMenu1' >";
+    for(var i = 0; i < propertyTypes.length; i++){
+        var propertyType = propertyTypes[i];
+        textForDropdown = textForDropdown + " <li role='presentation'><a role='menuitem' tabindex='-1' color='"+propertyType.color+"'  id='"+propertyType.id+"Prop"+this.lineNumber+"'><h4>"+propertyType.name+"</h4> -- "+propertyType.description+"</a></li>";
+
+    }
+    //append that text to the Property tag
+    $("#Properties" + this.lineNumber).append(textForDropdown + "</ul> </div>");
+
+
+    //For each label, set a 'click' listener
+    for(var i = 0; i < propertyTypes.length; i++){
+        //Listener definition
+        $("#" + propertyTypes[i].id + "Prop" + this.lineNumber ).click(function(){
+            //Pull the id, color, name, and lineNum out of the html element
+            var id = this.id.replace("Prop", "");
+            id = id.replace(/(\d+)/g, "");
+            var lineNum = this.id.replace(/([A-Z]+)/g, "");
+            lineNum = lineNum.replace(/([a-z]+)/g, "");
+            var name = $(this).text().split("--")[0];
+            var desc = $(this).text().split("--")[1]
+            var color = $(this).attr("color");
+
+            //Remove the button that adds that type of property
+            $(this).remove();
+            //if the list is empty, delete the button
+            if($("#PropertiesDropDown" + lineNum).has("a").length == 0){
+                $("#PropertiesDropDown" + lineNum).remove();
+            }
+
+            //Add the list-group for the property
+            $("#Properties"+lineNum).append("<ul class='list-group' id='"+id+"ListGroup"+lineNum+"'><li class='list-group-item' style='color:white; background-color: "+color+"; border-color: "+color+";'><h4 class='list-group-item-heading' style='color:white'>"+name+"</h4>"+desc+"</li><a  class='list-group-item' id='"+id+"PlusButton"+lineNum+"'>+</a></ul>");
+
+            //Add a 'click' listener for the plus button in that list group
+            //Logic for different formats is in the propertyTable Hash Table (defined in index)
+            $("#"+id+"ListGroup"+lineNum).on('click', 'a', function(){
+
+                //retrieve id and lineNum
+                var id = this.id.replace("PlusButton", "");
+                id = id.replace(/(\d+)/g, "");
+                var lineNum = this.id.replace(/([A-Z]+)/g, "");
+                lineNum = lineNum.replace(/([a-z]+)/g, "");
+
+                //Define the html code to be added, including a new plus button
+                var breadOne = "<li class='list-group-item'> <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button> ";
+                var insides = propertyTable[id];
+                var breadTwo = " </li> <a  class='list-group-item' id='"+id+"PlusButton"+lineNum+"'>+</a> ";
+
+                //append the code
+                $("#"+id+"ListGroup"+lineNum).append(breadOne + insides + breadTwo);
+
+                //remove the old plus button
+                var plusButton = $("#"+id+"PlusButton"+lineNum);
+                plusButton.remove();
+            });
+        });
+
+    }
+
+
+};
+
+//updates the lines properties every 100 ms
 LineOfDialogue.prototype.update = function(){
 
 
--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 function Main(){
 
+    //class which does a lot of the overall logic, keeps track of all lines of dialogue
+
 	//place initialization code here
 	//and here
 
 	this.currentLineNumber = 0;
 
 	this.linesOfDialogue = [];
-	this.linesOfDialogue.push(new LineOfDialogue(1));
+	this.addLine();
 
 }
 
+//updates all of the lines of dialogue every 100 ms
 Main.prototype.update = function(){
 	for(var lineNum = 0; lineNum < this.linesOfDialogue.length; lineNum++){
         var line = this.linesOfDialogue[lineNum];
@@ -17,6 +20,7 @@ Main.prototype.update = function(){
 	}
 };
 
+//Adds a new line of dialogue
 Main.prototype.addLine = function(){
     this.currentLineNumber++;
     this.linesOfDialogue.push(new LineOfDialogue(this.currentLineNumber));
@@ -25,29 +29,3 @@ Main.prototype.addLine = function(){
 
 
 
-/*
-Event listeners and html/jquery code goes here
-*/
-$(function(){
-
-    $(".dropdown-menu").on('click', 'li a', function(){
-      $(".btn:first-child").text($(this).text());
-      $(".btn:first-child").val($(this).text());
-   });
-
-});
-
-$(function(){
-    $(".list-group").on('click', 'a', function(){
-        var div = $(".list-group:parent");
-        var div2 =  $("a");
-        var partOne = "<li class='list-group-item'> <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button> ";
-        var insides = "Blue blue blue";
-        var partTwo = " </li> <a href='#' class='list-group-item'>+</a>";
-        div2.remove();
-        div.append(partOne + insides + partTwo);
-    });
-
-
-
-});
